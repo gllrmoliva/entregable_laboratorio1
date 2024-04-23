00f63dc82f57b22bec37f1089a4f83cd6eaf953a
Antonio Skorin <antonio.skorin@gmail.com>
2024-04-23 19:05:17 -0400

    Refactor: command.h movido a moving_image.h

--- Entregable 1/moving_image.h
+++ Entregable 1/moving_image.h
@@ -6,7 +6,71 @@
 
 #include <stack>
 #include <queue>
-#include "command.h"
+#include <algorithm>
+#include <string>
+
+// Enumerador de posibles comandos que se pueden hacer sobre la imagen.
+enum Command
+{
+    MOVE_LEFT = 0, // par
+    MOVE_RIGHT,
+    MOVE_DOWN, // par
+    MOVE_UP,
+    ROTATE, // par
+    UNDO_ROTATE
+};
+
+// Struct que define un movimiento en base al comando y su argumento.
+struct Movement
+{
+    Command command;
+    Command inv_command;
+    int arg;
+    Movement(Command command, int arg)
+    {
+        this->command = command;
+        this->arg = arg;
+        if (command % 2)
+        {
+            inv_command = (Command)(command - 1);
+        }
+        else
+        {
+            inv_command = (Command)(command + 1);
+        }
+    }
+
+    // Esto es solo para ver si se guardan bien las cosas en stack
+    std::string to_string()
+    {
+        switch (this->command)
+        {
+        case MOVE_LEFT:
+            return "IZQUIERDA - " + std::to_string(arg);
+            break;
+        case MOVE_RIGHT:
+            return "DERECHA - " + std::to_string(arg);
+            break;
+        case MOVE_UP:
+            return "ARRIBA - " + std::to_string(arg);
+            break;
+        case MOVE_DOWN:
+            return "ABAJO - " + std::to_string(arg);
+            break;
+        case ROTATE:
+            return "ROTAR 90";
+            break;
+        case UNDO_ROTATE:
+            return "ROTAR -90";
+            break;
+        default:
+            return "ALGO RARO OCURRIO";
+            break;
+        }
+    }
+};
+
+
 
 // Clase que representa una imagen como una colección de 3 matrices siguiendo el
 // esquema de colores RGB
--- Entregable 1/test.cpp
+++ Entregable 1/test.cpp
@@ -6,57 +6,21 @@
  */
 
 #include "moving_image.h"
-#include "command.h"
 #include <unistd.h> // para sleep (linux). Usar  #include<windows.h> para Windows
 
 int main()
 {
     moving_image im;
 
-    im.all_movements.push(Movement(ROTATE, 0));
-    im.all_movements.push(Movement(MOVE_RIGHT, 150));
-    im.all_movements.push(Movement(MOVE_UP, 200));
-    im.all_movements.push(Movement(MOVE_LEFT, 300));
     im.repeat_all();
 
-    im.print_stacks();
-    std::cout << std::endl;
-    im.draw("image1.png");
-
     im.rotate();
-    im.print_stacks();
-    std::cout << std::endl;
-    im.draw("image2.png");
-
-    im.undorotate();
-    im.print_stacks();
-    std::cout << std::endl;
-    im.draw("image3.png");
-
     im.move_right(100);
-    im.print_stacks();
-    std::cout << std::endl;
-    im.draw("image4.png");
-
     im.undo();
-    im.print_stacks();
-    std::cout << std::endl;
-    im.draw("image5.png");
-
     im.undo();
-    im.print_stacks();
-    std::cout << std::endl;
-    im.draw("image6.png");
-
     im.redo();
-    im.print_stacks();
-    std::cout << std::endl;
-    im.draw("image7.png");
-
     im.move_left(243);
-    im.print_stacks();
-    std::cout << std::endl;
-    im.draw("image8.png");
+    im.repeat_all();
 
     /* NOTA 1: Si usan el mismo nombre para las imágenes, entonces cada llamada al
     método draw() sobreescribirá a la imagen */
